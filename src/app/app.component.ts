b939a73a8a8b3591f8f5ddce792bcc1c4863f48c
Camila Santiago <92113919+Camilay3@users.noreply.github.com>
2025-08-08 10:36:43 -0300

    style (navbar): Template Desktop (#2)
    Criação do template desktop e posicionamento dos elementos com display grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CardComponent } from "./components/card/card.component";
+import { NavbarDesktopComponent } from "./components/navbar-desktop/navbar-desktop.component";
 
 @Component({
   selector: 'app-root',
-  imports: [CardComponent],
+  imports: [CardComponent, NavbarDesktopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/navbar-desktop/navbar-desktop.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-desktop',
+  imports: [],
+  templateUrl: './navbar-desktop.component.html',
+  styleUrl: './navbar-desktop.component.css'
+})
+export class NavbarDesktopComponent {
+  selecionado: boolean = false;
+}
