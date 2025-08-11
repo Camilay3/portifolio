4912772fbe9c1d313dcf71cfaba074534099a3a5
Camila Santiago <92113919+Camilay3@users.noreply.github.com>
2025-08-11 17:11:05 -0300

    style (Sobre): Tela 'Sobre mim' #fixed #6 (#3)
    Ajustes em elementos globais para auxiliar a reutilização de componentes e criação da tela 'Sobre mim'

--- src/app/pages/sobre-mim/sobre-mim.component.ts
+++ src/app/pages/sobre-mim/sobre-mim.component.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
+import { Icone } from '../../components/icones/icone';
+import { IconesComponent } from '../../components/icones/icones.component';
+
+interface Formacao {
+  src: string;
+  nome: string;
+  dataInical?: number;
+  dataFinal?: number;
+  status?: string;
+};
 
 @Component({
   selector: 'app-sobre-mim',
-  imports: [],
+  imports: [IconesComponent],
   templateUrl: './sobre-mim.component.html',
   styleUrl: './sobre-mim.component.css'
 })
 export class SobreMimComponent {
+  idade?: string;
+  dataNasc: Date = new Date(2006, 4, 2);
+  dataAtual?: Date;
 
+  ngOnInit() {
+    this.dataAtual = new Date();
+    this.idade = ((this.dataAtual.getTime() - this.dataNasc.getTime()) / 31557600000).toFixed(0);
+  }
+
+  formacoes: Formacao[] = [
+    {
+      src: "IFCE.svg",
+      nome: "Curso Técnico em Informática",
+      dataInical: 2021,
+      dataFinal: 2023
+    },
+    {
+      src: "IFCE.svg",
+      nome: "Bacharelado em Ciências da Computação",
+      status: "em andamento"
+    }
+  ];
+
+  hobbies: Icone[] = [
+    {src: "fi fi-ss-chess-king", label: "Enxadrista"},
+    {src: "fi fi-sr-violin", label: "Violinista"},
+    {src: "fas fa-palette", label: "Artesã"},
+  ];
 }
