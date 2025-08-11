645f2050430ee4565764e370287bf68bebb58265
Camila Santiago <92113919+Camilay3@users.noreply.github.com>
2025-08-11 20:27:22 -0300

    style (Projetos): Componente principal fixed #14 (#7)
    Criação do componente principal, configuração da exibição de imagens e listagem das tecnologias utilizadas

--- src/app/pages/projetos/projetos.component.ts
+++ src/app/pages/projetos/projetos.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+interface Projeto {
+  nome: string
+  descricao: string
+  tecnologia: string[]
+  url?: string
+  repositorio?: string
+  src?: string
+}
+
 @Component({
   selector: 'app-projetos',
   imports: [],
@@ -7,5 +16,21 @@ import { Component } from '@angular/core';
   styleUrl: './projetos.component.css'
 })
 export class ProjetosComponent {
+  hover?: boolean = false;
+  imagemExpandida?: boolean = false;
 
+  projetos?: Projeto[] = [
+    {
+      nome: "Devoday",
+      descricao: "Devocional day é um projeto estudantil que surgiu a partir da dificuldade de novos cristãos em fazer devocional e/ou lectio divina. O projeto também é pensado em cristãos que sabem fazer mas possuem dificuldades em manter constância nos estudos bíblicos.",
+      tecnologia: ["Node.js", "Javascript", "Handlebars", "CSS", "HTML", "PostgreSQL", "Nodemailer", "Prisma", "Express", "Bcrypt", "JWT"],
+      repositorio: "https://github.com/Camilay3/devoday",
+      url: "https://devoday.onrender.com",
+      src: "Devoday.png"
+    }
+  ];
+
+  expandir(estado?: boolean) {
+    this.imagemExpandida = (estado) ? true : false;
+  }
 }
